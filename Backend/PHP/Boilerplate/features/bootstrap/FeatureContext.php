52b0828657d76163f9b2af1bc134ab7531ba5f33
Jeremy Aubrun <jeremy.aubrun@gmail.com>
2024-04-16 21:56:33 +0200

    feat(BoilerPlate): Test Jeremy A.

--- Backend/PHP/Boilerplate/features/bootstrap/FeatureContext.php
+++ Backend/PHP/Boilerplate/features/bootstrap/FeatureContext.php
@@ -7,13 +7,18 @@ use Fulll\App\Calculator;
 
 class FeatureContext implements Context
 {
+
+    private array $vars;
+    
     /**
      * @When I multiply :a by :b into :var
      */
     public function iMultiply(int $a, int $b, string $var): void
     {
         $calculator = new Calculator();
-        $this->$var = $calculator->multiply($a, $b);
+        $value = $calculator->multiply($a, $b);
+        $this->setVar($var);
+        $this->setVarValue($var, $value);
     }
 
     /**
@@ -21,8 +26,26 @@ class FeatureContext implements Context
      */
     public function aShouldBeEqualTo(string $var, int $value): void
     {
-        if ($value !== $this->$var) {
-            throw new \RuntimeException(sprintf('%s is expected to be equal to %s, got %s', $var, $value, $this->$var));
+        if ($value !== $this->getVar($var)) {
+            throw new \RuntimeException(sprintf('%s is expected to be equal to %s, got %s', $var, $value, $this->getVar($var)));
         }
     }
+    
+
+    private function setVar(string $var): void
+    {
+        $this->vars[$var] = null;
+    }
+
+    private function getVar(string $var): int
+    {
+        return $this->vars[$var];
+    }
+
+    private function setVarValue(string $var, int $value): void
+    {
+        $this->vars[$var] = $value;
+    }
+
+
 }
